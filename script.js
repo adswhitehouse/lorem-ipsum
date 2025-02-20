574414d4d1f51173bda6b78dae85796f375350b9
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-20 21:30:02 +0000

    added alternative method or lorem generation in js and commented out first method

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
+// Import data array
 import { text } from "./lorem.js";
 
+//Element variables
 let paragraphInput = document.querySelector(".jsParagraphInput");
 let buttonInput = document.querySelector(".jsButtonInput");
 let randomTextContainer = document.querySelector(".jsRandomTextContainer");
 
+// Create lorem on button click
 buttonInput.addEventListener("click", () => {
-  let inputValue = paragraphInput.value;
-  if (inputValue === "") {
-    randomTextContainer.innerText = `${text[randomNumber()]}`;
-  } else if (inputValue >= 1 && inputValue <= 50) {
-    randomTextContainer.innerText = "";
-    for (let i = 0; i < inputValue; i++) {
-      randomTextContainer.innerText += `${text[randomNumber()]} \n \n`;
-    }
-  } else {
-    alert("Please input a valid paragraph generation value (1 - 50)");
-  }
-  paragraphInput.value = "";
+  createLorem();
 });
 
-function randomNumber() {
-  let number = Math.floor(Math.random() * text.length);
-  return number;
+// Create lorem on enter keydown
+paragraphInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    createLorem();
+  }
+});
+
+// Function to add lorem text to the page METHOD 1
+// function createLorem() {
+//   let inputValue = paragraphInput.value;
+//   if (inputValue >= 1 && inputValue <= 50) {
+//     randomTextContainer.innerText = "";
+//     for (let i = 0; i < inputValue; i++) {
+//       randomTextContainer.innerText += `${text[randomNumber()]} \n \n`;
+//     }
+//   } else {
+//     alert("Please input a valid paragraph generation value (1 - 50)");
+//   }
+//   paragraphInput.value = "";
+// }
+
+// function randomNumber() {
+//   let number = Math.floor(Math.random() * text.length);
+//   return number;
+// }
+
+// Function to add lorem text to the page METHOD 2
+function createLorem() {
+  let inputValue = paragraphInput.value;
+  if (inputValue < 1 || inputValue > text.length || isNaN(inputValue)) {
+    alert("Please enter a valid paragraph generation value (1 - 9)");
+  } else {
+    randomTextContainer.innerText = "";
+    let newTextArray = text.slice(0, inputValue);
+    newTextArray.forEach((item) => {
+      randomTextContainer.innerText += `${item} \n \n`;
+    });
+  }
 }
